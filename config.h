697c67955c6bc432780943851a3d57c840c21195
Ryan Barron <ryanbar1997@gmail.com>
2023-12-26 23:32:15 +0100

    Increase font size

--- config.h
+++ config.h
@@ -13,8 +13,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "IosekvaTerm Nerd Font:size=10" };
-static const char dmenufont[]       = "IosekvaTerm Nerd Font:size=10";
+static const char *fonts[]          = { "IosekvaTerm Nerd Font:size=14" };
+static const char dmenufont[]       = "IosekvaTerm Nerd Font:size=14";
 static unsigned int baralpha        = 0xd0;
 static unsigned int borderalpha     = OPAQUE;
 static const char col_gray1[]       = "#222222";
