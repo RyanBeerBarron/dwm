a9548301063e8a3c15c9391b7247462deaca3086
Ryan Barron <ryanbar1997@gmail.com>
2023-12-27 04:34:47 +0100

    Patch: dwm-colorbar-6.3.diff

--- config.h
+++ config.h
@@ -14,8 +14,13 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#2f2f55";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_cyan, col_gray1 },
 	[SchemeSel]  = { col_gray4, col_cyan,  "#00ff00"  },
+	[SchemeStatus]  = { col_gray4, "#1f1f28",  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { col_gray1, "#606080",  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+	[SchemeTagsNorm]  = { col_gray3, col_cyan,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+	[SchemeInfoSel]  = { col_gray4, col_cyan,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+	[SchemeInfoNorm]  = { col_gray3, col_cyan,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */
