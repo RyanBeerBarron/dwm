e9637f674ed1dd425c60371c19bfb3b4da624b5e
Ryan Barron <ryanbar1997@gmail.com>
2023-12-30 01:30:59 +0100

    config change

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "IosekvaTerm Nerd Font:size=14" };
-static const char dmenufont[]       = "IosekvaTerm Nerd Font:size=14";
+static const char *fonts[]          = { "IosekvaTerm Nerd Font:size=12" };
+static const char dmenufont[]       = "IosekvaTerm Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -14,12 +14,12 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#2f2f55";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_cyan, col_gray1 },
-	[SchemeSel]  = { col_gray4, col_cyan,  "#00ff00"  },
-	[SchemeStatus]  = { col_gray4, "#1f1f28",  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeNorm] = { col_gray1, col_cyan, col_gray1 },
+	[SchemeSel]  = { col_gray4, col_cyan,   "#606080"  },
+	[SchemeStatus]  = { col_gray4, col_cyan,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
 	[SchemeTagsSel]  = { col_gray1, "#606080",  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
 	[SchemeTagsNorm]  = { col_gray3, col_cyan,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
-	[SchemeInfoSel]  = { col_gray4, col_cyan,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+	[SchemeInfoSel]  = { col_gray1,  "#606080",  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
 	[SchemeInfoNorm]  = { col_gray3, col_cyan,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
@@ -105,11 +105,11 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_Left,   viewprev,       {0} },
 	{ MODKEY|ShiftMask,             XK_Right,  tagtonext,      {0} },
 	{ MODKEY|ShiftMask,             XK_Left,   tagtoprev,      {0} },
-    { 0,  XF86XK_MonBrightnessDown, spawn,  SHCMD("brightnessctl set 10%-;  pkill -36 dwmblocks") },
-    { 0,  XF86XK_MonBrightnessUp, spawn, SHCMD("brightnessctl set 10%+;  pkill -36 dwmblocks")},
+    { 0,  XF86XK_MonBrightnessDown, spawn,  SHCMD("brightnessctl set 5%-;  pkill -36 dwmblocks") },
+    { 0,  XF86XK_MonBrightnessUp, spawn, SHCMD("brightnessctl set 5%+;  pkill -36 dwmblocks")},
     { 0,  XF86XK_AudioMute, spawn, SHCMD( "amixer -D pulse set Master toggle; pkill -35 dwmblocks" )},
-    { 0,  XF86XK_AudioLowerVolume, spawn, SHCMD("amixer -D pulse set Master on; amixer -D pulse set Master 10%-; pkill -35 dwmblocks")},
-    { 0,  XF86XK_AudioRaiseVolume, spawn, SHCMD("amixer -D pulse set Master on; amixer -D pulse set Master 10%+; pkill -35 dwmblocks")},
+    { 0,  XF86XK_AudioLowerVolume, spawn, SHCMD("amixer -D pulse set Master on; amixer -D pulse set Master 5%-; pkill -35 dwmblocks")},
+    { 0,  XF86XK_AudioRaiseVolume, spawn, SHCMD("amixer -D pulse set Master on; amixer -D pulse set Master 5%+; pkill -35 dwmblocks")},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
